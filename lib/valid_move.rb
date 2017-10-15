eda7ee2af891e6be98fc87a14cb120e53f2a47ee
Vicky  T <vttak9@gmail.com>
2017-10-15 03:00:01 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,12 @@
 def valid_move?(board, index)
- if board[index] = "X"
-   false
- end
- if board[index] = "O"
+ if board[index] == "X"
     false
-  end
+ if board[index] == "O"
+    false
+ 
   elsif index < 9 && board[index] == " "
     true
-end
+ 
   elsif index < 9 && board[index] == ""
      true
    end
