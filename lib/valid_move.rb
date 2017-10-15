ca65980907c6474a8b75ab9337cddea372080386
Vicky  T <vttak9@gmail.com>
2017-10-15 02:53:07 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,7 @@
 def valid_move?(board, index)
-  if index < 9 && board[index] = " " || index < 9 && board[index] = ""
-  true
-elsif index < 9 && board[index] = "X" || index < 9 && board[index] = "O"
-  false
+ index < 9 && board[index] = " " || index < 9 && board[index] = ""
+
 
 
 end
-end
+
